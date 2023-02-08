a8fffd9331c5f87a972a53822c56a62a8e9bde7d
ache <48488540+acheiii@users.noreply.github.com>
2023-02-08 18:24:05 +0800

    Add files via upload

---
+++ BaiduFilesTransfers_Pro.py
@@ -0,0 +1,283 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import base64
+import tempfile
+import threading
+import time
+import webbrowser
+import zlib
+import os
+from bs4 import BeautifulSoup
+import sys
+import re
+from lxml import etree
+# noinspection PyCompatibility
+from tkinter import *
+import random
+import requests
+import urllib3
+from retrying import retry
+
+# 公共请求头
+request_header = {
+    'Host': 'pan.baidu.com',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+     'User_Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    'Sec-Fetch-Dest': 'document',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'Sec-Fetch-Site': 'same-site',
+    'Sec-Fetch-Mode': 'navigate',
+    'Referer': 'https://pan.baidu.com',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-US;q=0.7,en-GB;q=0.6,ru;q=0.5',
+}
+session = requests.session()
+urllib3.disable_warnings()
+s = requests.session()
+s.trust_env = False
+
+# 获取bdstoken函数
+def get_bdstoken():
+    url = 'https://pan.baidu.com/api/gettemplatevariable?clienttype=0&app_id=250528&web=1&fields=[%22bdstoken%22,%22token%22,%22uk%22,%22isdocuser%22,%22servertime%22]'
+    response = s.get(url=url, headers=request_header, timeout=20, allow_redirects=True, verify=False)
+    return response.json()['errno'] if response.json()['errno'] != 0 else response.json()['result']['bdstoken']
+
+# 获取目录列表函数
+def get_dir_list(bdstoken):
+    url = 'https://pan.baidu.com/api/list?order=time&desc=1&showempty=0&web=1&page=1&num=1000&dir=%2F&bdstoken=' + bdstoken
+    response = s.get(url=url, headers=request_header, timeout=15, allow_redirects=False, verify=False)
+    return response.json()['errno'] if response.json()['errno'] != 0 else response.json()['list']
+
+
+# 验证链接函数
+def check_links(link_url, pass_code, bdstoken):
+    # 验证提取码
+    if pass_code:
+        # 生成时间戳
+        t_str = str(int(round(time.time() * 1000)))
+        check_url = 'https://pan.baidu.com/share/verify?surl=' + link_url[25:48] + '&bdstoken=' + bdstoken + '&t=' + t_str + '&channel=chunlei&web=1&clienttype=0'
+        post_data = {'pwd': pass_code, 'vcode': '', 'vcode_str': '', }
+        response_post = s.post(url=check_url, headers=request_header, data=post_data, timeout=10, allow_redirects=False, verify=False)
+        # 在cookie中加入bdclnd参数
+        if response_post.json()['errno'] == 0:
+            bdclnd = response_post.json()['randsk']
+        else:
+            return response_post.json()['errno']
+        if bool(re.search('BDCLND=', request_header['Cookie'], re.IGNORECASE)):
+            request_header['Cookie'] = re.sub(r'BDCLND=(\S+);?', r'BDCLND=' + bdclnd + ';', request_header['Cookie'])
+        else:
+            request_header['Cookie'] += ';BDCLND=' + bdclnd
+    # 获取文件信息
+    response = s.get(url=link_url, headers=request_header, timeout=15, allow_redirects=True, verify=False).content.decode("utf-8")
+    shareid_list = re.findall('"shareid":(\\d+?),"', response)
+    user_id_list = re.findall('"share_uk":"(\\d+?)","', response)
+    fs_id_list = re.findall('"fs_id":(\\d+?),"', response)
+    info_title_list = re.findall('<title>(.+)</title>', response)
+    server_filedir = re.findall('"server_filename":"(.*?)",',response)
+    print("--------++++++++++++++++++","link_url: ",link_url, "shareid_list: ",shareid_list,"user_id_list: ",user_id_list,"fs_id_list: ", fs_id_list,"info_title_list: ",info_title_list, "server_filename: ",server_filedir)
+    if not shareid_list:
+        return 1
+    elif not user_id_list:
+        return 2
+    elif not fs_id_list:
+        return info_title_list[0] if info_title_list else 3
+    else:
+        return [shareid_list[0], user_id_list[0], fs_id_list, server_filedir[0]]
+
+
+# 转存文件函数
+def transfer_files(check_links_reason, dir_name, bdstoken):
+    url = 'https://pan.baidu.com/share/transfer?shareid=' + check_links_reason[0] + '&from=' + check_links_reason[1] + '&bdstoken=' + bdstoken + '&channel=chunlei&web=1&clienttype=0'
+    
+    fs_id = ','.join(i for i in check_links_reason[2])
+    post_data = {'fsidlist': '[' + fs_id + ']', 'path': '/' + dir_name, }
+    print(post_data, url)
+    response = s.post(url=url, headers=request_header, data=post_data, timeout=15, allow_redirects=False, verify=False)
+    return response.json()['errno']
+
+# 获取父目录下所以子文件夹及子文件
+#https://pan.baidu.com/share/list?uk=xxxxx&shareid=xxxxxxx&order=other&desc=1&showempty=0&web=1&page=1&num=100&dir=%2Fsecutity%2F
+def get_parentPath_and_subfiledir_subfile(user_id_list, shareid_list, parentPath, cookie):
+    fs_id_list = []
+    try:
+        print("开始获取父目录")
+        for page  in range(1,100):       
+            page = str(page)
+            url = "https://pan.baidu.com/share/list?uk=" + user_id_list + "&shareid=" + shareid_list + "&order=other&desc=1&showempty=0&web=1&page=" + page + "&num=100&dir=%2F" + parentPath + "%2F"
+            rsp = s.get(url=url, headers=request_header,  verify=False)
+            print(url)
+            fs_id = re.findall('"fs_id":(\\d*)',rsp.text)
+            fs_id_list.extend(fs_id)
+            if rsp.status_code != 200  or ('fs_id' not in rsp.text):
+                return fs_id_list
+    except Exception as e:
+        print("获取父子目录出错",e)
+
+# 多次转存文件函数
+@retry(stop_max_attempt_number=10, wait_fixed=1000)
+def sub_transfer_files(shareid, user_id, bdstoken, fs_id_list, dir_name):
+    url = 'https://pan.baidu.com/share/transfer?shareid=' + shareid + '&from=' + user_id + '&bdstoken=' + bdstoken + '&channel=chunlei&web=1&clienttype=0'
+    
+    fs_id = ''.join(fs_id_list)
+    post_data = {'fsidlist': '[' + fs_id + ']', 'path': '/' + dir_name, }
+    print(post_data, url)
+    response = s.post(url=url, headers=request_header, data=post_data, timeout=25, allow_redirects=False, verify=False)
+    return response.json()['errno']
+
+# # 多线程
+# def thread_it(func, *args):
+#     t = threading.Thread(target=func, args=args, daemon=True)
+#     t.start()
+#     # t.join()
+
+# 检测链接种类
+def check_link_type(link_list_line):
+    if link_list_line.find('https://pan.baidu.com/s/') >= 0:
+        link_type = '/s/'
+    return link_type
+
+# 新建目录函数
+def create_dir(dir_name, bdstoken):
+    url = 'https://pan.baidu.com/api/create?a=commit&bdstoken=' + bdstoken
+    post_data = {'path': dir_name, 'isdir': '1', 'block_list': '[]', }
+    response = s.post(url=url, headers=request_header, data=post_data, timeout=15, allow_redirects=False, verify=False)
+    print(post_data)
+    return response.json()['errno']
+
+# 主程序
+def main():
+    # 获取和初始化数据
+    dir_name = "".join(input('\033[2;32m输入文件名: \033[0m\n'))
+    cookie = "".join(input('\033[2;32m输入cookie: \033[0m\n'))
+    request_header['Cookie'] = cookie
+    link_url = "".join(input('\033[2;32m输入网盘链接: 例如https://pan.baidu.com/s/1fcW_xxxxxxxxxxxxxxx 提取码: 2ejd \n\r\r\r\t 或者 https://pan.baidu.com/s/1fcW_U-AYdXXXX-XXXX  2ejd\033[0m\n'))
+    # pass_code = "".join(input('\033[2;32m输入网盘密码: \033[0m\n'))
+    # pass_code = "2ejd"
+
+    # 开始运行函数
+    try:
+        # 检查cookie输入是否正确
+        if any([ord(word) not in range(256) for word in cookie]) or cookie.find('BAIDUID=') == -1:
+            print('百度网盘cookie输入不正确,请检查cookie后重试.' + '\n')
+            sys.exit()
+
+        # 执行获取bdstoken
+        bdstoken = get_bdstoken()
+        print(bdstoken,"bdstoken-------------------------------------")
+        if isinstance(bdstoken, int):
+            print('没获取到bdstoken,错误代码:' + str(bdstoken) + '\n')
+            sys.exit()
+
+        # 执行获取本人网盘目录列表
+        dir_list_json = get_dir_list(bdstoken)
+        if type(dir_list_json) != list:
+            print('没获取到网盘目录列表,请检查cookie和网络后重试.' + '\n')
+            sys.exit()
+
+        # shareid_list, user_id_list, fs_id_list, server_filedir=check_links(link_url, pass_code, bdstoken)
+        # check_links_results=check_links(link_url, pass_code, bdstoken)
+        # shareid_list=check_links_results[0] 
+        # user_id_list=check_links_results[1]
+        # fs_id_list=check_links_results[2]   
+        # server_filedir=check_links_results[3]   
+        # 执行新建目录
+        dir_list = [dir_json['server_filename'] for dir_json in dir_list_json]
+        if dir_name and dir_name not in dir_list:
+            create_dir_reason = create_dir(dir_name, bdstoken)
+            if create_dir_reason != 0:
+                print('文件夹名带非法字符,请改正文件夹名称后重试.' + '\n')
+                sys.exit()
+
+        # 执行转存
+        # 处理http链接
+        link_url = link_url.replace("http://", "https://")
+        # 处理(https://pan.baidu.com/s/1tU58ChMSPmx4e3-kDx1mLg?pwd=123w)格式链接
+        link_url = link_url.replace("?pwd=", " ")
+        # 处理旧格式链接
+        link_url = link_url.replace("https://pan.baidu.com/share/init?surl=", "https://pan.baidu.com/s/1")
+        # 判断连接类型
+        link_type = check_link_type(link_url)
+        # 处理(https://pan.baidu.com/s/1tU58ChMSPmx4e3-kDx1mLg 123w)格式链接
+        if link_type == '/s/':
+            link_url_org, pass_code_org = re.sub(r'提取码*[：:](.*)', r'\1', link_url.lstrip()).split(' ', maxsplit=1)
+            [link_url, pass_code] = [link_url_org.strip()[:47], pass_code_org.strip()[:4]]
+            shareid_list,user_id_list,fs_id_list,server_filedir = check_links(link_url, pass_code, bdstoken)
+
+            # 执行检查链接有效性
+            check_links_reason = check_links(link_url, pass_code, bdstoken)
+        #     if check_links_reason == 1:
+        #         print('链接失效,没获取到shareid:' + link_url + '\n')
+        #     elif check_links_reason == 2:
+        #         print('链接失效,没获取到user_id:' + link_url + '\n')
+        #     elif check_links_reason == 3:
+        #         print('链接失效,没获取到fs_id:' + link_url + '\n')
+        #     elif check_links_reason == '百度网盘-链接不存在':
+        #         print('链接失效,文件已经被删除或取消分享:' + link_url + '\n')
+        #     elif check_links_reason == '百度网盘 请输入提取码':
+        #         print('链接错误,缺少提取码:' + link_url + '\n')
+        #     elif check_links_reason == -12:
+        #         print('链接错误,提取码错误:' + link_url + '\n')
+        #     elif check_links_reason == -62:
+        #         print('链接错误尝试次数过多,请手动转存或稍后再试:' + link_url + '\n')
+        #     elif check_links_reason == 105:
+        #         print('链接错误,链接格式不正确:' + link_url + '\n')
+        #     elif isinstance(check_links_reason):
+            # 执行转存文件
+            transfer_files_reason = transfer_files(check_links_reason, dir_name, bdstoken)
+            if transfer_files_reason == 0:
+                print('转存成功:' + link_url + '\n')
+            elif transfer_files_reason == -4:
+                print('转存失败,无效登录.请退出账号在其他地方的登录:' + link_url + '\n')
+            elif transfer_files_reason == 4 or transfer_files_reason == -8:
+                print('转存失败,目录中已有同名文件或文件夹存在:' + link_url + '\n')
+            elif transfer_files_reason == 12:
+                print('转存失败,转存文件数超过限制:' + link_url + '\n', "开始分批转存......")
+                #https://pan.baidu.com/share/list?uk=&shareid=&order=other&desc=1&showempty=0&web=1&page=1&num=100&dir=%2Fsecutity%2F
+                print(user_id_list, shareid_list, server_filedir)
+                new_fs_id_list = get_parentPath_and_subfiledir_subfile(user_id_list, shareid_list, server_filedir, cookie)
+                print(new_fs_id_list)
+                if new_fs_id_list != "":
+                    for fs_id_list in new_fs_id_list:
+                        time.sleep(round(random.uniform(0.001, 5.01), 3))
+                        print(fs_id_list,"sleeptime: ",round(random.uniform(0.001, 5.01), 3))
+                        sub_transfer_files(shareid_list,user_id_list, bdstoken, fs_id_list, dir_name)
+                        
+
+            else:
+                print('转存失败,错误代码(' + str(transfer_files_reason) + '):' + link_url + '\n')
+        else:
+            print('访问链接返回错误代码(' + str(check_links_reason) + '):' + link_url + '\n')
+
+    #         transfer_files_reason = transfer_files_rapid(rapid_data, dir_name, bdstoken)
+    #         if transfer_files_reason == 0:
+    #             print('转存成功:' + url_code + '\n')
+    #         elif transfer_files_reason == -8:
+    #             print('转存失败,目录中已有同名文件或文件夹存在:' + url_code + '\n')
+    #         elif transfer_files_reason == -9:
+    #             print('验证已过期,请重新获取cookie:' + url_code + '\n')
+    #         elif transfer_files_reason == 404:
+    #             print('转存失败,秒传无效:' + url_code + '\n')
+    #         elif transfer_files_reason == -10:
+    #             print('转存失败,容量不足:' + url_code + '\n')
+    #         elif transfer_files_reason == 2:
+    #             print('转存失败,请重试:' + url_code + '\n')
+    #         else:
+    #             print('转存失败,错误代码(' + str(transfer_files_reason) + '):' + url_code + '\n')
+    #     elif link_type == 'unknown':
+    #         print('不支持链接:' + url_code + '\n')
+    #     task_count = task_count + 1
+    #     label_state_change(state='running', task_count=task_count, task_total_count=task_total_count)
+    except Exception as e:
+        print('运行出错,请重新运行本程序.错误信息如下:' + '\n')
+        print(str(e) + '\n')
+        print('用户输入内容:' + '\n')
+        print('百度Cookies:' + cookie + '\n')
+        print('文件夹名:' + dir_name + '\n')
+        print('链接输入:' + '\n' + str(link_url))
+        
+    # 恢复按钮状态
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,35 @@
+# BaiduFilesTransfers_Pro
+------------------------------------------
+###### 百度网盘批量转存工具
+
+`python3  BaiduFilesTransfers_Pro.py`
+
+`文件名`   --指本人网盘转存的文件夹名（默认根路径 /）
+`cookie`  --网盘登录的cookie
+`网盘链接`  --格式如  					
+
+- https://pan.baidu.com/s/XXX_XXXX-XXXXXXX提取码: 2ejd
+-   https://pan.baidu.com/s/XXX_XXXX-XXXXXXX    2ejd
+![](./img/Pastedimage20230208165025.png)
+
+
+#### 01 来源介绍
+--------------------------------------------------------
+某日看到某云盘资料多，随上同性交友网站寻求转存脚本
+
+得一老哥脚本 
+# BaiduPanFilesTransfers（https://github.com/hxz393/BaiduPanFilesTransfers）
+有会员转存歧视
+
+遂据老哥脚本改了改，遵循够用就行原理
+
+
+#### 02 接前文cookie
+---------------------------------------------
+###### Cookie    F12  找到cookie  复制
+![](./img/Pastedimage20230208173248.png)
+
+
+#### 02 后言
+--------------------------------------------
+或许会升级，前提是有时间或者需要用到        -  -
